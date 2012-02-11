9f0c65372ccc0c53fb238d9bd150e3326f2201ee
Teodor Pripoae <teodor.pripoae@gmail.com>
2012-02-11 12:58:50 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Censormenot::Application.config.secret_token = 'e4520788895617b95c9b2d28d93fadeb1f553870331305c82446d5581895515c94d23c727605b3055e1325d3d1f66056fe1bf90451dd21f0e08cad106f166eb1'
