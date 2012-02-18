87192fd6fd1d50e0168c970f9f41d7be38501ebe
Teodor Pripoae <teodor.pripoae@gmail.com>
2012-02-18 16:21:40 +0200

    Added aggregate function for staged records

--- app/models/staged_record.rb
+++ app/models/staged_record.rb
@@ -1,3 +1,18 @@
 class StagedRecord < ActiveRecord::Base
   validates_uniqueness_of :answered_by, :scope => :domain
+
+  def self.aggregate
+    groups = group("domain", "ip")
+
+    groups.each do |group|
+      p "Domain: #{group.domain} #{group.ip}"
+      records = where(:domain => group.domain, :ip => group.ip)
+      average = records.collect(&:trust).sum.to_f/records.length
+
+      average = (average + 0.5) / 2 if records.length < 10
+
+      lookup = Lookup.find_by_domain_and_ip(group.domain, group.ip)
+      lookup.update_attributes(:trust => average)
+    end
+  end
 end
