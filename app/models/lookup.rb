dd4e3cabfb169a3519884af586c084f1ed898ac6
Teodor Pripoae <teodor.pripoae@gmail.com>
2012-02-18 14:21:55 +0200

    Changed to 3.2.1 added validation for domain+ip for lookup

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,4 +1,10 @@
 class Lookup < ActiveRecord::Base
   validates :domain, :presence => true
   validates :ip, :presence => true
+
+  #validates_uniqueness_of :ip, :score => [:domain]
+
+  before_validation(:on => :create) do
+    return Lookup.where(:domain => domain, :ip => ip).size == 0
+  end
 end
